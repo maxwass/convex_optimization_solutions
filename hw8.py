edc26150b93cc148db7588fe4b8b9433e6bdda77
Max Wasserman <maxw14k@gmail.com>
2022-05-13 14:54:45 -0400

    part 2 of hw8 complete.

--- hw8.py
+++ hw8.py
@@ -67,9 +67,10 @@ def backtrack(x: np.ndarray, dir: np.ndarray, grad: np.ndarray, c:np.ndarray,
     return t
 
 
-def newton_method_centering(A: np.ndarray, c: np.ndarray, x_0: np.ndarray,
-                            alpha: float = .1, beta: float = .5,
-                            eps: float = 1e-6):
+def center(A: np.ndarray, c: np.ndarray, x_0: np.ndarray,
+           alpha: float = .1, beta: float = .5, eps: float = 1e-6):
+    # perform a 'centering' step in the barrier method
+
     # we assume x_0 is feasible
 
     objective_values, newton_decrements, step_sizes, flops = [], [], [], []
@@ -78,15 +79,15 @@ def newton_method_centering(A: np.ndarray, c: np.ndarray, x_0: np.ndarray,
         obj, grad = f(c, x), c - 1/x
         newton_step, nu = newton_step_block_elim(A, x, c)
         newton_decr_sq = - (newton_step.T @ grad)/2  # recommended formula from assignment
-        t = backtrack(x, newton_step, grad, c, alpha, beta)
-        x = x + t * newton_step
+        step_size = backtrack(x, newton_step, grad, c, alpha, beta)
+        x = x + step_size * newton_step
 
         # log for plotting
         objective_values.append(obj)
         newton_decrements.append(newton_decr_sq)
-        step_sizes.append(t)
+        step_sizes.append(step_size)
 
-        print(f'{len(objective_values)}: objective {obj:.5f}, nd: {newton_decr_sq:.5f}, t: {t:.5f}')
+        #print(f'{len(objective_values)}: objective {obj:.5f}, nd: {newton_decr_sq:.5f}, t: {t:.5f}')
         #print(f'\tcondition of hess: {np.linalg.cond(hess):.5f}')
 
     info = {'objective_values': np.array(objective_values),
@@ -97,17 +98,82 @@ def newton_method_centering(A: np.ndarray, c: np.ndarray, x_0: np.ndarray,
 
     return x, nu, info
 
+
 #### Part 2: LP solver with strictly feasible starting point. ####
+
+
+def lp_barrier_method(A: np.ndarray, c: np.ndarray, x_0: np.ndarray, # problem params
+                      alpha: float = .1, beta: float = .5, # backtrack params
+                      eps: float = 1e-3,
+                      mu: float = 10, t_0: float = 1): # barrier method params
+    # barrier method to solve an LP
+
+    n = len(x_0)
+
+    steps, duality_gap = [], []
+    x, t, i = x_0, t_0, 0
+    while i < 50: # never do more than 50 centering steps.
+        x, nu, info = center(A=A, c=t*c, x_0=x, alpha=alpha, beta=beta, eps=1e-6)
+        duality_gap.append(n/t)
+        steps.append(info['num_steps'])
+        print(f'{i}: primal_obj: {c.T @ x:.3f}, duality gap {duality_gap[-1]:.4f}, steps {steps[-1]}, t {t:.2f}')
+
+        if duality_gap[-1] < eps:
+            break
+
+        t = t * mu
+        i = i + 1
+
+    #history = np.stack()
+
+    return x, steps, duality_gap
+
+
 
 #### Part 3 ####
 
 
 if __name__ == "__main__":
-
-    # sample problem data
-    A, b, x_0, c = sample_problem_data(m=500, n=2000, seed=50)
+    # sample problem data to be used for all examples below
+    A, b, x_0, c = sample_problem_data(m=100, n=500, seed=50)
     alpha, beta = .1, .2
-    x, nu, info = newton_method_centering(A, c, x_0, alpha=alpha, beta=beta, eps=1e-6)
+
+    ### Part 1: centering ###
+    x, nu, info = center(A, c, x_0, alpha=alpha, beta=beta, eps=1e-6)
+
+    ### Part 2: Test barrier method by comparing to CVX ###
+    x = cp.Variable(len(x_0))
+    prob = cp.Problem(cp.Minimize(c.T @ x),
+                      [A @ x == b, x >= 0])
+    prob.solve()  # verbose=True)
+
+    x_custom, steps, duality_gap = \
+        lp_barrier_method(A=A, c=c, x_0=x_0,
+                          alpha=alpha, beta=beta,
+                          eps=1e-3,
+                          mu=10, t_0=10)
+    print(f'CVX: primal value {prob.value:.4f}, Our Impl: primal value {c.T @ x_custom:.4f}')
+
+    ### Part 2: Plot mu's influence on total netwon iterations ###
+    fig, ax = plt.subplots()
+    for mu in [2, 10, 50, 100]:
+        x_custom, steps, duality_gap = \
+            lp_barrier_method(A=A, c=c, x_0=x_0,
+                              alpha=alpha, beta=beta,
+                              eps=1e-3,
+                              mu=mu, t_0=10)
+        plt.step(np.cumsum(steps), np.log10(duality_gap), where='post', label=f'mu={mu}')
+    ax.set_xlabel('Newton Iterations'); ax.set_ylabel('duality gap (log)'); ax.legend()
+    plt.title(f"Mu's influence on total Newton Iterations"); plt.show()
+    print('hi')
+    ### Part 3: General LP solver ###
+
+
+
+
+
+
+
 
     # do returned x, nu approximately solve the kkt conditions?
     # optimality: grad Lagrangian = 0?
